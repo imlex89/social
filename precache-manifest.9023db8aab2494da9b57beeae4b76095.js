cf90876dd5823f499f779af99d222715b9c9fcd9
zagrick <zagrick18@mail.ru>
2020-06-10 22:14:38 +0300

    add files react

---
+++ precache-manifest.9023db8aab2494da9b57beeae4b76095.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac81e563d5f7282876a8d38e6f9aa49",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "b909dae598575b73fff3",
+    "url": "/social/static/css/3.e5eef49c.chunk.css"
+  },
+  {
+    "revision": "809f250cacbdf1d7aaf9",
+    "url": "/social/static/css/4.deb1b7c1.chunk.css"
+  },
+  {
+    "revision": "c364c6da73550c4e32d5",
+    "url": "/social/static/css/main.502a0840.chunk.css"
+  },
+  {
+    "revision": "39534dea195d255172d2",
+    "url": "/social/static/js/2.99dee669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social/static/js/2.99dee669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b909dae598575b73fff3",
+    "url": "/social/static/js/3.8387fec2.chunk.js"
+  },
+  {
+    "revision": "809f250cacbdf1d7aaf9",
+    "url": "/social/static/js/4.e1131f4f.chunk.js"
+  },
+  {
+    "revision": "c364c6da73550c4e32d5",
+    "url": "/social/static/js/main.a8a5e27b.chunk.js"
+  },
+  {
+    "revision": "40bd314e6603c13118e4",
+    "url": "/social/static/js/runtime-main.76f1cbdb.js"
+  },
+  {
+    "revision": "07689d4eaaa3d530d58826b5d7f84735",
+    "url": "/social/static/media/Montserrat.07689d4e.ttf"
+  },
+  {
+    "revision": "0eca307eb2541dea2098c4da23958f49",
+    "url": "/social/static/media/Montserrat.0eca307e.woff"
+  },
+  {
+    "revision": "2c2616f3064a0aa92a1be68366c26e9a",
+    "url": "/social/static/media/Montserrat.2c2616f3.svg"
+  },
+  {
+    "revision": "c330043b3f9ace258d44c975f2a98b2a",
+    "url": "/social/static/media/Montserrat.c330043b.eot"
+  },
+  {
+    "revision": "f9675a7abb93b4a17f93ea9cff47a9cf",
+    "url": "/social/static/media/logo.f9675a7a.svg"
+  },
+  {
+    "revision": "9c1fa513f850ea31c678abe2c652296b",
+    "url": "/social/static/media/preloader.9c1fa513.svg"
+  },
+  {
+    "revision": "25429a1349f49adabdc9e289d6e61720",
+    "url": "/social/static/media/user.25429a13.png"
+  }
+]);
